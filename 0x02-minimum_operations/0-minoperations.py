aa69db3672a691925574d78ec1bbe1a43575e028
Ouazane <mohamedouazane@yahoo.com>
2024-04-23 14:35:06 -0700

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 0. Minimum Operations """
+
+
+def minOperations(n):
+    """method that calculates the fewest
+    number of operations needed to result in exactly
+    n H characters in the file."""
+    ope = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+            ope += 2
+        else:
+            n = n - 1
+            ope += 1
+    return ope
