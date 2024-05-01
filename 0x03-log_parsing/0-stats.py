440e9890b389af0c79920f6784946baa7dda5abc
Ouazane <mohamedouazane@yahoo.com>
2024-05-01 15:17:28 -0700

    1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,21 +3,25 @@ import sys
 from collections import Counter
 
 status = []
-for _ in range(10000):
-    elements = []
-    size = 0
-    try:
-        for _ in range(10):
-            data = input()
+elements = {}
+size = 0
+try:
+    for _ in range(10000):
+        i = 0
+        for line in sys.stdin:
+            i += 1
+            data = line.rstrip()
             parts = data.split()
             size += int(parts[8])
             status.append(parts[7])
             elements = Counter(status)
+            if i == 10:
+                break
         print(f"File size: {size}")
         for i, k in elements.items():
             print(f"{i}: {k}")
         status.clear()
-    except KeyboardInterrupt:
-        print(f"File size: : {size}")
-        for i, k in elements.items():
-            print(f"{i}: {k}")
+except KeyboardInterrupt:
+    print(f"File size: : {size}")
+    for i, k in elements.items():
+        print(f"{i}: {k}")
