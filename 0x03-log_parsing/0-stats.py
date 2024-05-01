b60478d09026588f49c62a58e97289365a51f848
Ouazane <mohamedouazane@yahoo.com>
2024-05-01 15:44:20 -0700

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,27 +3,25 @@
 import sys
 from collections import Counter
 
-
 status = []
 elements = {}
 size = 0
+sortlist = []
+
 try:
-    for _ in range(10000):
-        i = 0
-        for line in sys.stdin:
-            i += 1
-            data = line.rstrip()
-            parts = data.split()
-            size += int(parts[8])
-            status.append(parts[7])
-            elements = Counter(status)
-            if i == 10:
-                break
-        print(f"File size: {size}")
-        sortlist = sorted(elements.items(), key=lambda x: x[0])
-        for i, k in sortlist:
-            print(f"{i}: {k}")
-        status.clear()
+    for line in sys.stdin:
+        data = line.rstrip()
+        parts = data.split()
+        size += int(parts[8])
+        status.append(parts[7])
+        elements = Counter(status)
+        if len(status) == 10:
+            print(f"File size: {size}")
+            sortlist = sorted(elements.items(), key=lambda x: x[0])
+            for i, k in sortlist:
+                print(f"{i}: {k}")
+            status.clear()
+
 except KeyboardInterrupt:
     print(f"File size: : {size}")
     for i, k in sortlist:
