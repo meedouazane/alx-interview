11b31ba360f3178388df5955247b1f37b4776699
Ouazane <mohamedouazane@yahoo.com>
2024-05-09 04:11:33 -0700

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,23 +8,29 @@ def validUTF8(data):
     :param data: data set (can contain multiple characters)
     :return: True if data is a valid UTF-8 encoding, else return False
     """
-    for i in range(len(data)):
-        if data[i] & 0b11000000 == 0b11000000:
-            if not ((data[i + 1]) & 0b11000000 == 0b10000000
-                    and (data[i + 2]) & 0b11000000 == 0b10000000):
-                return False
-            i += 2
-        if (data[i]) & 0b11100000 == 0b11100000:
-            if not ((data[i + 1]) & 0b11000000 == 0b10000000 and
-                    (data[i + 2]) & 0b11000000 == 0b10000000 and
-                    (data[i + 3]) & 0b11000000 == 0b10000000):
-                return False
-            i += 3
-        if data[i] & 0b11110000 == 0b11110000:
-            if not (data[i + 1] & 0b11000000 == 0b10000000 and
-                    data[i + 2] & 0b11000000 == 0b10000000 and
-                    data[i + 3] & 0b11000000 == 0b10000000 and
-                    data[i + 4] & 0b11000000 == 0b10000000):
-                return False
-            i += 4
+    i = 0
+    while i < len(data):
+        try:
+            if data[i] & 0b11000000 == 0b11000000:
+                if not ((data[i + 1]) & 0b11000000 == 0b10000000
+                        and (data[i + 2]) & 0b11000000 == 0b10000000):
+                    return False
+                i += 2
+            elif (data[i]) & 0b11100000 == 0b11100000:
+                if not ((data[i + 1]) & 0b11000000 == 0b10000000 and
+                        (data[i + 2]) & 0b11000000 == 0b10000000 and
+                        (data[i + 3]) & 0b11000000 == 0b10000000):
+                    return False
+                i += 3
+            elif data[i] & 0b11110000 == 0b11110000:
+                if not (data[i + 1] & 0b11000000 == 0b10000000 and
+                        data[i + 2] & 0b11000000 == 0b10000000 and
+                        data[i + 3] & 0b11000000 == 0b10000000 and
+                        data[i + 4] & 0b11000000 == 0b10000000):
+                    return False
+                i += 4
+            else:
+                i += 1
+        except IndexError:
+            return False
     return True
