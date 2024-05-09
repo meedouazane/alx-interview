5c8dad40a56d03a2375329d824990d4ed977b671
Ouazane <mohamedouazane@yahoo.com>
2024-05-09 06:02:43 -0700

    0-validate_utf8.py5

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,20 +13,25 @@ def validUTF8(data):
     i = 0
     while i < len(data):
         if data[i] & 0b11100000 == 0b11000000:
-            if len(data) < i + 2 or not (
-                    data[i + 1] & 0b11000000 == 0b10000000
+            if len(data) <= i + 2:
+                return False
+            if not (data[i + 1] & 0b11000000 == 0b10000000
                     and data[i + 2] & 0b11000000 == 0b10000000):
                 return False
             i += 2
         elif data[i] & 0b11110000 == 0b11100000:
-            if len(data) < i + 3 or not (
+            if len(data) <= i + 3:
+                return False
+            if not (
                     data[i + 1] & 0b11000000 == 0b10000000 and
                     data[i + 2] & 0b11000000 == 0b10000000 and
                     data[i + 3] & 0b11000000 == 0b10000000):
                 return False
             i += 3
         elif data[i] & 0b11111000 == 0b11110000:
-            if len(data) < i + 4 or not (
+            if len(data) <= i + 4:
+                return False
+            if not (
                     data[i + 1] & 0b11000000 == 0b10000000 and
                     data[i + 2] & 0b11000000 == 0b10000000 and
                     data[i + 3] & 0b11000000 == 0b10000000 and
