59b38cb29450dfa5165e1bc40b2832d1c2ce0e77
Ouazane <mohamedouazane@yahoo.com>
2024-05-09 04:39:35 -0700

    0-validate_utf8-2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,18 +17,18 @@ def validUTF8(data):
                 return False
             i += 2
         elif data[i] & 0b11100000 == 0b11100000:
-            if len(data) < i + 3 or not (
-                    data[i + 1] & 0b11000000 == 0b10000000
-                    and data[i + 2] & 0b11000000 == 0b10000000
-                    and data[i + 3] & 0b11000000 == 0b10000000):
+            if len(data) <= i + 3 or not (
+                    data[i + 1] & 0b11000000 == 0b10000000 and
+                    data[i + 2] & 0b11000000 == 0b10000000 and
+                    data[i + 3] & 0b11000000 == 0b10000000):
                 return False
             i += 3
         elif data[i] & 0b11110000 == 0b11110000:
-            if len(data) < i + 4 or not (
-                    data[i + 1] & 0b11000000 == 0b10000000
-                    and data[i + 2] & 0b11000000 == 0b10000000
-                    and data[i + 3] & 0b11000000 == 0b10000000
-                    and data[i + 4] & 0b11000000 == 0b10000000):
+            if len(data) <= i + 4 or not (
+                    data[i + 1] & 0b11000000 == 0b10000000 and
+                    data[i + 2] & 0b11000000 == 0b10000000 and
+                    data[i + 3] & 0b11000000 == 0b10000000 and
+                    data[i + 4] & 0b11000000 == 0b10000000):
                 return False
             i += 4
         else:
