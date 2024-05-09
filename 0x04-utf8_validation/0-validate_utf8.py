9296d79a05117d97b8b1f4a32d8666478fa1692b
Ouazane <mohamedouazane@yahoo.com>
2024-05-09 08:31:52 -0700

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,29 +3,33 @@
 
 
 def validUTF8(data):
+    """
+    UTF-8 Validation
+    :param data: data set (can contain multiple characters)
+    :return: True if data is a valid UTF-8 encoding, else return False
+    """
     i = 0
     while i < len(data):
         try:
-            if (data[i] & 0b10000000) == 0:  # 1-byte character
+            if (data[i] & 0b10000000) == 0:
                 i += 1
-            elif (data[i] & 0b11100000) == 0b11000000:  # 2-byte character
+            elif (data[i] & 0b11100000) == 0b11000000:
                 if (data[i + 1] & 0b11000000) != 0b10000000:
                     return False
                 i += 2
-            elif (data[i] & 0b11110000) == 0b11100000:  # 3-byte character
+            elif (data[i] & 0b11110000) == 0b11100000:
                 if (data[i + 1] & 0b11000000) != 0b10000000 or \
                    (data[i + 2] & 0b11000000) != 0b10000000:
                     return False
                 i += 3
-            elif (data[i] & 0b11111000) == 0b11110000:  # 4-byte character
+            elif (data[i] & 0b11111000) == 0b11110000:
                 if (data[i + 1] & 0b11000000) != 0b10000000 or \
                    (data[i + 2] & 0b11000000) != 0b10000000 or \
                    (data[i + 3] & 0b11000000) != 0b10000000:
                     return False
                 i += 4
             else:
-                return False  # Invalid starting byte
+                return False
         except IndexError:
-            return False  # Incomplete UTF-8 sequence
+            return False
     return True
-
