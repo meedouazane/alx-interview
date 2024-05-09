4439f331955dd8c3aeb4ba965b93589f24a4e248
Ouazane <mohamedouazane@yahoo.com>
2024-05-09 08:29:47 -0700

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,34 +3,29 @@
 
 
 def validUTF8(data):
-    """
-    UTF-8 Validation
-    :param data: data set (can contain multiple characters)
-    :return: True if data is a valid UTF-8 encoding, else return False
-    """
     i = 0
     while i < len(data):
         try:
-            if data[i] & 0b11000000 == 0b11000000:
-                if not ((data[i + 1]) & 0b11000000 == 0b10000000
-                        and (data[i + 2]) & 0b11000000 == 0b10000000):
+            if (data[i] & 0b10000000) == 0:  # 1-byte character
+                i += 1
+            elif (data[i] & 0b11100000) == 0b11000000:  # 2-byte character
+                if (data[i + 1] & 0b11000000) != 0b10000000:
                     return False
                 i += 2
-            elif (data[i]) & 0b11100000 == 0b11100000:
-                if not ((data[i + 1]) & 0b11000000 == 0b10000000 and
-                        (data[i + 2]) & 0b11000000 == 0b10000000 and
-                        (data[i + 3]) & 0b11000000 == 0b10000000):
+            elif (data[i] & 0b11110000) == 0b11100000:  # 3-byte character
+                if (data[i + 1] & 0b11000000) != 0b10000000 or \
+                   (data[i + 2] & 0b11000000) != 0b10000000:
                     return False
                 i += 3
-            elif data[i] & 0b11110000 == 0b11110000:
-                if not (data[i + 1] & 0b11000000 == 0b10000000 and
-                        data[i + 2] & 0b11000000 == 0b10000000 and
-                        data[i + 3] & 0b11000000 == 0b10000000 and
-                        data[i + 4] & 0b11000000 == 0b10000000):
+            elif (data[i] & 0b11111000) == 0b11110000:  # 4-byte character
+                if (data[i + 1] & 0b11000000) != 0b10000000 or \
+                   (data[i + 2] & 0b11000000) != 0b10000000 or \
+                   (data[i + 3] & 0b11000000) != 0b10000000:
                     return False
                 i += 4
             else:
-                i += 1
+                return False  # Invalid starting byte
         except IndexError:
-            return False
+            return False  # Incomplete UTF-8 sequence
     return True
+
