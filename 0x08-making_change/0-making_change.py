2bbf2e67e707deb81b92780c8b13c41a57df5963
Ouazane <mohamedouazane@yahoo.com>
2024-06-04 09:41:29 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,14 +12,15 @@ def makeChange(coins, total):
     :return: number of coins needed
     """
     count = 0
-    i = len(coins) - 1
+    i = 0
+    coins.sort(reverse=True)
     if total <= 0:
         return 0
-    while i >= 0:
+    while i <= len(coins) - 1:
         while total > coins[i]:
             total -= coins[i]
             count += 1
-        i -= 1
+        i += 1
     if total != 1:
         return -1
     return count
