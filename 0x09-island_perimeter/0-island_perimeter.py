23b58685eef1bf4736fcbafca4e7c6c35c618cdc
Meed <mohamedouazane@yahoo.com>
2024-06-13 11:51:09 +0000

    3

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,7 +7,7 @@ Island Perimeter
 def island_perimeter(grid):
     """
     Find the perimeter of the island
-    :param grid: list of list of integers represent island
+    :param grid: list of list of integers representing the island
     :return: the perimeter of the island
     """
     perimeter = 0
@@ -15,15 +15,16 @@ def island_perimeter(grid):
         return 0
     row = len(grid)
     col = len(grid[0])
+
     for i in range(row):
         for g in range(col):
             if grid[i][g] == 1:
-                if grid[i - 1][g] == 0 and i == 0:
+                if i == 0 or grid[i - 1][g] == 0:
                     perimeter += 1
-                if grid[i + 1][g] == 0 and i == row - 1:
+                if i == row - 1 or grid[i + 1][g] == 0:
                     perimeter += 1
-                if grid[i][g - 1] == 0 and g == 0:
+                if g == 0 or grid[i][g - 1] == 0:
                     perimeter += 1
-                if grid[i][g + 1] == 0 and g == col - 1:
+                if g == col - 1 or grid[i][g + 1] == 0:
                     perimeter += 1
     return perimeter
